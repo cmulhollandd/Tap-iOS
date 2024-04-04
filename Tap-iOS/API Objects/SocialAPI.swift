7709135ea352bce5cf8482988a21406fda6e46a3
Charlie Mulholland <charliemul04@gmail.com>
2024-04-04 12:36:30 -0500

    Added SocialAPI class to handle all social actions. Added method stub for follow user

---
+++ Tap-iOS/API Objects/SocialAPI.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAPI.swift
+//  Tap-iOS
+//
+//  Created by Charlie Mulholland on 4/4/24.
+//
+
+import Foundation
+
+class SocialAPI: NSObject {
+    
+    private static let baseAPIURL = "https://rhodestap.com"
+    
+    public static func followUser(_ username: String, completion: @escaping([String: Any]) -> Void) {
+        
+    }
+}
