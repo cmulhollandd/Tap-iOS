b3f9a49a887b729375ccf71d29ef98b6aac432b6
Charlie Mulholland <charliemul04@gmail.com>
2024-04-15 15:08:24 -0500

    Small Tweaks to animation timing

--- Tap-iOS/View Controllers/OnboardingVCs/LaunchScreenViewController.swift
+++ Tap-iOS/View Controllers/OnboardingVCs/LaunchScreenViewController.swift
@@ -29,7 +29,7 @@ class LaunchScreenViewController: UIViewController {
         let seconds = 2.0
         DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
             self.animationView!.loopMode = .playOnce
-            self.animationView!.animationSpeed = 1.0
+            self.animationView!.animationSpeed = 1.5
             self.animationView!.play(completion: { _ in
                 self.performSegue(withIdentifier: "LoginViewController", sender: nil)
             })
